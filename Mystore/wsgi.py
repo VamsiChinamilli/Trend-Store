f3b9a01fb337b5c54b14273b3c9cf177aaf29340
Vamsi Chinamilli <nagavamsichinamilli@gmail.com>
2026-03-23 22:15:13 +0530

    updated3

--- Mystore/wsgi.py
+++ Mystore/wsgi.py
@@ -1,17 +1,22 @@
-"""
-WSGI config for Mystore project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+# 1. Get the absolute path of the directory containing wsgi.py
+# (This is your inner 'Mystore' folder)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 2. Get the path of the parent directory (the root where manage.py lives)
+project_root = os.path.dirname(current_dir)
+
+# 3. Add both to the system path so Django can 'see' itself
+if project_root not in sys.path:
+    sys.path.append(project_root)
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
+# 4. Point to your settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mystore.settings')
 
 application = get_wsgi_application()
-app = application
+app = application  # Critical for Vercel
